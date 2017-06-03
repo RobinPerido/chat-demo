a3a7947cbc6ff339af2c302afafe1f31637439e8
vagrant <vagrant@homestead>
2017-06-03 19:51:41 +0000

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Robin',
+            'email' => 'ledungsbacken@gmail.com',
+            'password' => bcrypt('hundgodis1'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Test',
+            'email' => 'test@test.se',
+            'password' => bcrypt('test123'),
+        ]);
+    }
+}
---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<div>
+		<p>{{ message.message }}</p>
+		<small>{{ message.user.name }}</small>
+	</div>
+</template>
+
+<script>
+export default {
+  	props: ['message']
+
+};
+</script>
+
+<style lang="css" scoped>
+</style>
